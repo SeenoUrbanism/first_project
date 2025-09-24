009b995d6f13f8230b993ceb4b551ce06f1737fc
luispabloaiello-da <luispablo.aiello@gmail.com>
2025-09-24 10:43:37 +0200

    functions.py

--- notebooks/functions.py
+++ notebooks/functions.py
@@ -6,35 +6,46 @@
 # Remember to modify your functions to use the template shown below
 
 import pandas as pd
-
-'''
-def function_name(input1: data_type1, input2: data_type2,..., opt_arg: data_type_opt= default_value) -> output_data_type:
-	"""
-	Add a description of what the function does
-
-	Arguments:
-	---------
-	input1: data_type and which information should contain
-	input2: data_type and which information should contain
-	opt_arg: data_type and which information should contain
-
-	Outputs:
-	-------
-	data_type and which information will contain
-	"""
-	
-	# Your function code here
-
-	
-	return opuput
-'''
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import datetime as dt
+import re
 
 # Standardize column names
-def standardize_columns(df: pd.DataFrame) -> pd.DataFrame:
-    df.columns = df.columns.str.strip().str.replace(' ', '_')
+def standardize_column_names(df: pd.DataFrame) -> pd.DataFrame:
+    df.columns = df.columns.str.strip().str.replace(' ', '_').str.lower()
     return df
 
-
+# Drop duplicate rows .
 def drop_duplicates(df: pd.DataFrame, column) -> pd.DataFrame: 
     return df.drop_duplicates(subset=[column])
-     
+
+# Merge two pandas DataFrames with customizable parameters.
+def concat_dataframes(left_df, right_df):
+	return pd.concat([left_df,right_df], ignore_index = True)
+
+# Remove all punctuation and all special characters (keeps only letters, numbers, and spaces)
+def remove_all_punctuation(df: pd.DataFrame, columns) -> pd.DataFrame:
+    df[columns] = df[columns].map(lambda x: re.sub(r'[^A-Za-z0-9 ]+', '', x) if isinstance(x, str) else x)
+    return df
+
+# Drop unnecessary columns
+def drop_irrelevant_columns(df: pd.DataFrame, columns) -> pd.DataFrame:
+    return df.drop(columns=columns, axis=1, inplace=True, errors="ignore")
+
+# Filters rows in 'column' of the dataframe that match the provided regex pattern.
+def filter_by_regex_pattern(df: pd.DataFrame, column, regex_pattern: str) -> pd.DataFrame:
+    mask = df[column].str.contains(regex_pattern, flags=re.IGNORECASE, na=False, regex=True)
+    return df[mask].copy().reset_index(drop=True)
+
+def standardize_dates(df: pd.DataFrame, column) -> pd.DataFrame:
+    # df[column] = pd.to_datetime(df[column], format="%d-%b-%Y", errors='coerce')
+    
+    # Make a copy of df
+    df_merged_dates = df.copy()
+    # Convert 'post_until' to datetime (with your format)
+    df_merged_dates['post_until'] = pd.to_datetime(df_merged_dates['post_until'], format="%d-%b-%Y", errors='coerce')
+    # Format 'post_until' as string in 'dd-mm-YYYY'
+    df_merged_dates['post_until'] = df_merged_dates['post_until'].dt.strftime('%d-%m-%Y')
+    return df_merged_dates
