a89b96553f54b886a3a766295c5df9568c1e9ba1
luispabloaiello-da <luispablo.aiello@gmail.com>
2025-09-24 18:07:56 +0200

    EDA Analysis

--- notebooks/functions.py
+++ notebooks/functions.py
@@ -28,6 +28,9 @@ def concat_dataframes(left_df, right_df):
 # Remove all punctuation and all special characters (keeps only letters, numbers, and spaces)
 def remove_all_punctuation(df: pd.DataFrame, columns) -> pd.DataFrame:
     df[columns] = df[columns].map(lambda x: re.sub(r'[^A-Za-z0-9 ]+', '', x) if isinstance(x, str) else x)
+    # Lowercase All Categorical/Text Columns and Remove Extra Spaces
+    for col in columns:
+        df[col] = df[col].str.lower().str.strip().str.replace(r'\s+', ' ', regex=True)
     return df
 
 # Drop unnecessary columns
@@ -37,17 +40,29 @@ def drop_irrelevant_columns(df: pd.DataFrame, columns) -> pd.DataFrame:
 
 # Filters rows in 'column' of the dataframe that match the provided regex pattern.
 def filter_by_regex_pattern(df: pd.DataFrame, column, regex_pattern: str) -> pd.DataFrame:
-    df[column] = df[column].lower()
+    df[column] = df[column].str.lower()
     mask = df[column].str.contains(regex_pattern, flags=re.IGNORECASE, na=False, regex=True)
     return df[mask].copy().reset_index(drop=True)
 
-def standardize_dates(df: pd.DataFrame, column) -> pd.DataFrame:
-    # df[column] = pd.to_datetime(df[column], format="%d-%b-%Y", errors='coerce')
-    
-    # Make a copy of df
-    df_merged_dates = df.copy()
-    # Convert 'post_until' to datetime (with your format)
-    df_merged_dates[column] = pd.to_datetime(df_merged_dates[column], format="%d-%b-%Y", errors='coerce')
-    # Format 'post_until' as string in 'dd-mm-YYYY'
-    df_merged_dates[column] = df_merged_dates[column].dt.strftime('%d-%m-%Y')
-    return df_merged_dates
+# Standardize All Date Columns as datetime objects for analysis
+def standardize_dates(df: pd.DataFrame, columns) -> pd.DataFrame:
+    for col in columns:
+        df[col] = pd.to_datetime(df[col], errors='coerce', dayfirst=True)
+    return df
+
+# def standardize_dates(df: pd.DataFrame, column) -> pd.DataFrame:
+#     # Make a copy of df
+#     df_merged_dates = df.copy()
+#     # Convert 'post_until' to datetime (with your format)
+#     df_merged_dates[column] = pd.to_datetime(df_merged_dates[column], format="%d-%b-%Y", errors='coerce')
+#     # Format 'post_until' as string in 'dd-mm-YYYY'
+#     df_merged_dates[column] = df_merged_dates[column].dt.strftime('%d-%m-%Y')
+#     return df_merged_dates
+
+# regex_pattern = r"(sql|tableau|bi|phyton|eda|llm|ai|ml|pandas|NumPy|Agile)"
+# # Apply re.findall to each row in the 'Preferred_Skills' column
+# keyword_matches = df_merged['Preferred_Skills'].apply(lambda x: re.findall(regex_pattern, x, flags=re.IGNORECASE) if isinstance(x, str) else [])
+# # Filter rows where at least one keyword was found
+# df_keywords = df_merged[keyword_matches.apply(lambda matches: len(matches) > 0)].copy().reset_index(drop=True)
+
+# df_keywords[[columns]] = df_keywords[[columns]].map(lambda x: re.sub(r'[^A-Za-z0-9 ]+', '', x) if isinstance(x, str) else x)
